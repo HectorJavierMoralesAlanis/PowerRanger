4ba864773daf59334e07eca27ab9052e3fd7c425
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-11-26 22:10:56 -0600

    version 8

--- ejemplo1.py
+++ ejemplo1.py
@@ -1,13 +1,9 @@
-import _mssql
+import mysql.connector
 
-server = 'localhost'
-user = 'admin'
-password = '20c655592b90f29474c9fb6c04d39c83f1fb2249c93d27be'
-database = 'rfid'
-conn = _mssql.connect(server, user, password, database)
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="admin",
+  password="0c655592b90f29474c9fb6c04d39c83f1fb2249c93d27be"
+)
 
-
-conn.execute_query('SELECT * FROM pets WHERE name=%s', 'alumnos')
-
-for row in conn:
-    print ("ID=%d, Name=%s" % (row['id'], row['name']))
+print(mydb)
