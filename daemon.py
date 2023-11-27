30b039cb42e4c05569cd35424904a635de972fcd
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-11-26 22:38:07 -0600

    version 18

---
+++ daemon.py
@@ -0,0 +1,12 @@
+import os
+import time
+print('getcwd: ', os.getcwd())
+print('__file__:   ',__file__)
+import subprocess
+
+
+ruta = "/var/www/html/ejemplo.py"
+#time.sleep(60)
+while aux == 0:
+    time.sleep(1)
+    subprocess.run(['python',ruta])
