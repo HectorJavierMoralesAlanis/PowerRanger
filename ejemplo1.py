107632df604b6aa15afcef8e0fcde94182d3b463
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-11-26 22:18:42 -0600

    version 10

--- ejemplo1.py
+++ ejemplo1.py
@@ -1,8 +1,30 @@
 import sqlite3
-con = sqlite3.connect("rfid.db")
 
+# Connect to the SQLite database
+con = sqlite3.connect("rfid.db")
 cur = con.cursor()
 
+try:
+    # Check if the "alumnos" table exists
+    cur.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='alumnos';")
+    table_exists = cur.fetchone()
 
-res = cur.execute("SELECT * FROM alumnos")
-res.fetchone()
+    if table_exists:
+        # If the table exists, fetch a record from it
+        cur.execute("SELECT * FROM alumnos")
+        result = cur.fetchone()
+
+        if result:
+            print(result)
+        else:
+            print("Table 'alumnos' is empty.")
+    else:
+        print("Table 'alumnos' does not exist.")
+
+except sqlite3.Error as e:
+    print(f"SQLite error: {e}")
+
+finally:
+    # Close the cursor and connection
+    cur.close()
+    con.close()
