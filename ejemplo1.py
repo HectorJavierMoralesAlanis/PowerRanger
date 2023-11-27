87d3c600aede6b73ca39ca8c631cd97fabdf25e1
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-11-26 22:34:22 -0600

    version 15

--- ejemplo1.py
+++ ejemplo1.py
@@ -3,10 +3,10 @@ import mysql.connector
 # Replace these values with your own database connection details
 db_config = {
     # Replace these values with your MySQL server credentials
-    host = "localhost"
-    user = "admin"
-    password = "20c655592b90f29474c9fb6c04d39c83f1fb2249c93d27be"
-    database = "rfid"
+    'host': 'local',
+    'user': 'admin',
+    'password': '20c655592b90f29474c9fb6c04d39c83f1fb2249c93d27be',
+    'database': 'rfid'
 }
 
 try:
